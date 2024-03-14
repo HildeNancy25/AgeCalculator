c962469b8626479a633c2cf951d60098561495c4
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-03-14 12:05:39 +0200

    last updates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // var theDate = document.getElementById("date");
 // const theButton = document.getElementById("calc");
 const theOutput = document.getElementById("result");
+const AgeError = document.getElementById("date");
 
 theOutput.innerHTML = "...your age will be calculated soon";
 
@@ -8,8 +9,12 @@ function displayAge () {
     var birthdate = new Date(document.getElementById("date").value);
     let today = new Date();
     var result = today.getFullYear() - birthdate.getFullYear();
-
-    document.getElementById("result").innerHTML = "Your age is " + result + " years old";
+    let errormsg = AgeError.value;
+    if (errormsg === ""){
+        document.getElementById("result").innerHTML = "Please make sure you have entered your age!";
+    } else {
+        document.getElementById("result").innerHTML = "Your age is " + result + " years old";
+    }
 }
 
 // date.addEventListener("click" , displayAge);
